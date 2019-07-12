237a977c8898cd683b70ca664902b139775e4674
joeface71 <joefacenda@yahoo.com>
2019-07-12 15:55:22 -0400

    trails and prefab variants

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -68,7 +68,21 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) return;
-            target.TakeDamage(currentWeapon.GetDamage());
+
+            if (currentWeapon.HasProjectile())
+            {
+                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target);
+            }
+            else
+            {
+                target.TakeDamage(currentWeapon.GetDamage()); 
+            }            
+        }
+
+        // Animation event -- called within animator
+        void Shoot()
+        {
+            Hit();
         }
 
         private bool GetIsInRange()
